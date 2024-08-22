d3639f179f64fa4528ddb8f374eb11f1624c2588
Dawid Pachura <david.pachura@gmail.com>
2024-08-21 23:18:13 -0500

    created basic grpc api

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-A gRPC API created using Rust
+# A Basic gRPC API Created with Rust
+
+**Steps to install needed dependencies to run:**
+
+```shell
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Protobuf compiler
+sudo apt install protobuf-compiler
+
+# Install Protobuf header and lib
+sudo apt install libprotobuf-dev
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+use api::my_service_server::{MyService, MyServiceServer};
+use api::{HelloRequest, HelloResponse};
+
+pub mod api {
+    tonic::include_proto!("api");
+}
+
+#[derive(Default)]
+pub struct MyServiceImpl{}
+
+#[tonic::async_trait]
+impl MyService for MyServiceImpl {
+    async fn greet(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloResponse>, Status> {
+        let reply = HelloResponse {
+            message: format!("Hello {}", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "[::1]:50051".parse()?;
+    let my_service = MyServiceImpl::default();
+
+    Server::builder()
+        .add_service(MyServiceServer::new(my_service))
+        .serve(address)
+        .await?;
+
+    Ok(())
 }
